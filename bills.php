d4092257e2243cb9c472474288d02731633eb2be
havihavish <havihavi008@gmail.com>
2016-10-13 12:50:13 +0530

    bills,php added by havish

---
+++ bills.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+	<div>
+  		<form action="dealer.php">
+   		   <input type="Submit" value="dealer">
+  		</form>
+
+  		<form action="customer.php">
+    	   <input type="Submit" value="customer">
+  		</form>
+	</div>
+</body>
+</html>
